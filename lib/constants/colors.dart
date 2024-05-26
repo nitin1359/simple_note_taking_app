8ca34bcd98437f60207fdef3917c38bf0170e745
nitin1359 <nitinshukla135@gmail.com>
2024-05-26 13:24:19 +0530

    Version 2

---
+++ lib/constants/colors.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+
+List<Color> backgroundColors = [
+  const Color(0xFFCCE5FF), // light blue
+  const Color(0xFFD7F9E9), // pale green
+  const Color(0xFFFFF8E1), // pale yellow
+  const Color(0xFFF5E6CC), // beige
+  const Color(0xFFFFD6D6), // light pink
+  const Color(0xFFE5E5E5), // light grey
+  const Color(0xFFFFF0F0), // pale pink
+  const Color(0xFFE6F9FF), // pale blue
+  const Color(0xFFD4EDDA), // mint green
+  const Color(0xFFFFF3CD), // pale orange
+  const Color(0xFFFAE1DD), // light coral
+  const Color(0xFFEBDEF0), // light lavender
+  const Color(0xFFF0F4C3), // pale lime
+  const Color(0xFFE3F2FD), // very light blue
+  const Color(0xFFFFEBEE), // very light red
+  const Color(0xFFE8F5E9), // very light green
+  const Color(0xFFFFFDE7), // very light yellow
+  const Color(0xFFEDE7F6), // very light purple
+  const Color(0xFFFFE0B2), // light apricot
+  const Color(0xFFE0F7FA), // light cyan
+  const Color(0xFFF8BBD0), // light pink rose
+  const Color(0xFFDCEDC8), // light greenish yellow
+  const Color(0xFFFFF9C4), // light lemon
+  const Color(0xFFF5F5F5), // very light grey
+];
